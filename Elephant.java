48ac0bcd2149328677a31b05b1d305eb339ee6ca
polo2325 <flipflop2325@outlook.com>
2024-09-19 13:03:30 -0400

    Allow user to control elephant

--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,13 @@ public class Elephant extends Actor
      */
     public void act()
     {
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-10);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(10);
+        }
     }
 }
