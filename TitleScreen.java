d2c894ed7f63ee99f64ce06c841149c2c056e38f
polo2325 <flipflop2325@outlook.com>
2024-09-24 23:06:12 -0400

    add title screen

---
+++ TitleScreen.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title Screen.
+ * 
+ * @author Marco Yu
+ * @version 24 September 2024
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("The Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2); 
+    }
+    
+    /**
+     * The main world act loop
+     */
+    public void act()
+    {
+         // Start the game if user presses the space bar
+         if(Greenfoot.isKeyDown("space"))
+         {
+             MyWorld gameWorld = new MyWorld();
+             Greenfoot.setWorld(gameWorld);
+         }
+    }
+}
